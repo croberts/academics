74ec44c3523b32ec327888f9f67b1c102751afd2
Chris Roberts <ispeakzebra@gmail.com>
2022-06-02 09:55:00 -0500

    works

--- problem_sets/leetcode/684.py
+++ problem_sets/leetcode/684.py
@@ -24,23 +24,59 @@ The given graph is connected.
 from collections import defaultdict
 
 class Solution:
-    def findRedundantConnection(self, edges):
-      edge_dict = defaultdict(list)
-      last_edge = None
-      for edge in edges:
-        from_node = edge[0]
-        to_node = edge[1]
+  visited = []
 
-        if from_node in edge_dict and to_node in edge_dict:
-          last_edge = edge
+  def findRedundantConnection(self, edges):
+    edge_dict = defaultdict(list)
+    redundant_edges = []
+    self.visited = []
 
-        edge_dict[from_node].append(to_node)
-        edge_dict[to_node].append(from_node)
+    for edge in edges:
+      from_node = edge[0]
+      to_node = edge[1]
 
-      print(edge_dict)
-      print(last_edge)
+      if from_node in edge_dict and to_node in edge_dict:
+        redundant_edges.append(edge)
 
-      return last_edge
+      edge_dict[from_node].append(to_node)
+      edge_dict[to_node].append(from_node)
+
+    last_edge = self.try_remove(edge_dict, redundant_edges)
+
+    #print(edge_dict)
+    #print(last_edge)
+
+    return last_edge
+
+  def try_remove(self, edge_dict, redundant_edges):
+    i = len(redundant_edges) - 1
+    no_islands = False
+
+    while i > -1:
+      edge = redundant_edges[i]
+      edge_dict[edge[0]].remove(edge[1])
+      edge_dict[edge[1]].remove(edge[0])
+
+      no_islands = self.dfs(edge_dict, next(iter(edge_dict)))
+
+      if no_islands:
+        return edge
+      else:
+          edge_dict[edge[0]].append(edge[1])
+          edge_dict[edge[1]].append(edge[0])
+          self.visited = []
+      i -= 1
+
+  def dfs(self, edge_dict, root_node):
+    #print(edge_dict)
+    #print("root: ", root_node)
+    self.visited.append(root_node)
+
+    #print(self.visited)
+    for new_node in edge_dict[root_node]:
+      if new_node not in self.visited:
+        self.dfs(edge_dict, new_node)
+    return set(self.visited) == set(edge_dict.keys())
 
 
 
@@ -51,7 +87,7 @@ edge_dict_2 = [[1,2],[2,3],[3,4],[1,4],[1,5]]
 edge_dict_3 = [[1,3],[3,4],[1,5],[3,5],[2,3]]
 edge_dict_4 = [[9,10],[5,8],[2,6],[1,5],[3,8],[4,9],[8,10],[4,10],[6,8],[7,9]]
 
-solution = s.findRedundantConnection(edge_dict_4)
+solution = s.findRedundantConnection(edge_dict_2)
 print(solution)
 
 """
@@ -68,4 +104,10 @@ print(solution)
 
 Problem:
 if you remove the 6-8 connection you no longer have a connected graph w/ 2.
+
+When looking at a new problem, I'd say attack a test case w/ the complexity
+you're most familiar with, + 1 level. Not max, not min. Min takes too long
+exhastively to reach the highest level, and highest level may be too
+complex that you don't make any progress.
+
 """
